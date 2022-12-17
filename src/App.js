8eff9692543305e5795ba96adc3c41a8fcb4dca2
Ali Al-Zuhairi <zohairi@live.com>
2022-12-17 21:33:48 +0200

    adding the index.html and the routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import { Route, Routes } from 'react-router-dom';
-import Main from "./pages/Main";
+import Header from "./pages/Header";
+import Home from "./pages/Home";
+import Portfolio from "./pages/Portfolio";
+import Services from "./pages/Services";
+import About from "./pages/About";
+import Footer from "./pages/Footer";
 
 function App() {
   return (
     <>
+      <Header />
       <Routes>
-        <Route path="/" element={<Main />} />
+        <Route path="/header" element={<Header />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/footer" element={<Footer />} />
       </Routes>
+      <Footer />
     </>
 
-);
+  );
 }
 
 export default App;
