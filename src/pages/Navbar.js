a192264ce73708836a5ba38501c3002154161ad1
Ali Al-Zuhairi <zohairi@live.com>
2022-12-20 12:28:39 +0200

    update active

--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -1,7 +1,7 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import "../App.css";
 
 function BasicExample() {
   return (
@@ -11,7 +11,7 @@ function BasicExample() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/" className='active'>Home</Nav.Link>
+            <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="/portfolio">Portfolio</Nav.Link>
             <Nav.Link href="/services">Services</Nav.Link>
             <Nav.Link href="/about">About</Nav.Link>
