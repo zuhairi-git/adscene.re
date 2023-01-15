8b952def43436e1d2f18930d639c3d7658a8f57e
Ali Al-Zuhairi <zohairi@live.com>
2023-01-15 09:49:53 +0200

    Update Navbar.js

--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -8,18 +8,17 @@ function BasicExample() {
     <Navbar sticky="top" className='brand-light-color-bg' expand="lg">
       <Container>
         <Navbar.Brand href="/"><img src='./assets/img/adscene-logo.png' style={{ width: '50px' }} /></Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="me-auto"></Nav>
-            <Nav>
-              <Nav.Link href="/">Home</Nav.Link>
-              <Nav.Link href="/portfolio">Portfolio</Nav.Link>
-              <Nav.Link href="/services">Services</Nav.Link>
-              <Nav.Link href="/about">About</Nav.Link>
-              <Nav.Link href="#footer">Contact</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto"></Nav>
+          <Nav>
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/portfolio">Portfolio</Nav.Link>
+            <Nav.Link href="/services">Services</Nav.Link>
+            <Nav.Link href="/about">About</Nav.Link>
+            <Nav.Link href="#footer">Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
